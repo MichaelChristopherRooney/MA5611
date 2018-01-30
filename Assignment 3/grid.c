7569978d1cf31336cba5d8e4daa6175999a0c2b0
m <m.rooney93@gmail.com>
2018-01-30 03:53:43 -0800

    Progress on assignment 3

--- Assignment 3/grid.c
+++ Assignment 3/grid.c
@@ -36,72 +36,52 @@ static void create_grid(){
 	for(i = 0; i < LOCAL_NROWS; i++){
 		prev_grid[i] = &(temp[LOCAL_NCOLS * i]);
 	}
-	int count = 1;
+	// For debug to see where things are being transfered
+	int count = 11;
 	int n;
-	for(i = 0; i < LOCAL_NROWS; i++){
-		for(n = 0; n < LOCAL_NCOLS; n++){
-			grid[i][n] = count;
+	for(i = 1; i < LOCAL_NROWS - 1; i++){
+		for(n = 1; n < LOCAL_NCOLS - 1; n++){
+			//grid[i][n] = count;
 			count++;
 		}
 	}
 }
 
-#define LEFT_VALUE 175
-#define RIGHT_VALUE 125
-#define TOP_VALUE 200
-#define BOTTOM_VALUE 150
+// TODO change these back when done debugging
+#define LEFT_VALUE 10
+#define RIGHT_VALUE 10
+#define TOP_VALUE 10
+#define BOTTOM_VALUE 10
 
 void init_grid(){
 	create_grid();
 	int i;
 	// Node touches the bottom
 	if(LOCAL_Y_COORD == MPI_CART_DIMS[Y_INDEX] - 1){
-		int limit;
-		int start;
-		// Don't overwrite left/right edges
-		// TODO: > 2 horizontal divisions
-		if(LOCAL_X_COORD == MPI_CART_DIMS[X_INDEX] - 1){
-			limit = LOCAL_NCOLS - 1;
-			start = 0;
-		} else {
-			limit = LOCAL_NCOLS;
-			start = 1;
-		}
-		for(i = start; i < limit; i++){
-			grid[LOCAL_NROWS-1][i] = BOTTOM_VALUE;
-			prev_grid[LOCAL_NROWS-1][i] = BOTTOM_VALUE;
+		for(i = 1; i < LOCAL_NCOLS - 1; i++){
+			grid[LOCAL_NROWS-2][i] = BOTTOM_VALUE;
+			prev_grid[LOCAL_NROWS-2][i] = BOTTOM_VALUE;
 		}
 	}
 	// Node touches the top
 	if(LOCAL_Y_COORD == 0){
-		int limit;
-		int start;
-		// Don't overwrite left/right edges
-		// TODO: > 2 horizontal divisions
-		if(LOCAL_X_COORD == MPI_CART_DIMS[X_INDEX] - 1){
-			limit = LOCAL_NCOLS - 1;
-			start = 0;
-		} else {
-			limit = LOCAL_NCOLS;
-			start = 1;
-		}
-		for(i = start; i < limit; i++){
-			grid[0][i] = TOP_VALUE;
-			prev_grid[0][i] = TOP_VALUE;
+		for(i = 1; i < LOCAL_NCOLS - 1; i++){
+			grid[1][i] = TOP_VALUE;
+			prev_grid[1][i] = TOP_VALUE;
 		}
 	}
 	// Node touches the left side
 	if(LOCAL_X_COORD == 0){
-		for(i = 0; i < LOCAL_NROWS; i++){
-			grid[i][0] = LEFT_VALUE;
-			prev_grid[i][0] = LEFT_VALUE;
+		for(i = 1; i < LOCAL_NROWS - 1; i++){
+			grid[i][1] = LEFT_VALUE;
+			prev_grid[i][1] = LEFT_VALUE;
 		}
 	}
 	// Node touches the right side
 	if(LOCAL_X_COORD == MPI_CART_DIMS[X_INDEX] - 1){
-		for(i = 0; i < LOCAL_NROWS; i++){
-			grid[i][LOCAL_NCOLS-1] = RIGHT_VALUE;
-			prev_grid[i][LOCAL_NCOLS-1] = RIGHT_VALUE;
+		for(i = 1; i < LOCAL_NROWS - 1; i++){
+			grid[i][LOCAL_NCOLS-2] = RIGHT_VALUE;
+			prev_grid[i][LOCAL_NCOLS-2] = RIGHT_VALUE;
 		}
 	}
 }
--- Assignment 3/parallel.c
+++ Assignment 3/parallel.c
@@ -1,27 +1,5 @@
 #include "common.h"
 
-// Note: only allocates buffers if they are needed.
-// Example: does not allocate a left recv buffer if the local nodes is touching
-// the left side of the cart grid.
-static void init_recv_bufs(){
-	if(LOCAL_Y_COORD != 0){
-		//printf("Rank %d: allocating top buf\n", RANK);
-		TOP_RECV_BUF = calloc(LOCAL_NCOLS, sizeof(double));
-	}
-	if(LOCAL_Y_COORD != MPI_CART_DIMS[Y_INDEX] - 1){
-		//printf("Rank %d: allocating bottom buf\n", RANK);
-		BOTTOM_RECV_BUF = calloc(LOCAL_NCOLS, sizeof(double));
-	}
-	if(LOCAL_X_COORD != 0){
-		//printf("Rank %d: allocating left buf\n", RANK);
-		LEFT_RECV_BUF = calloc(LOCAL_NROWS, sizeof(double));
-	}
-	if(LOCAL_X_COORD != MPI_CART_DIMS[X_INDEX] - 1){
-		//printf("Rank %d: allocating right buf\n", RANK);
-		RIGHT_RECV_BUF = calloc(LOCAL_NROWS, sizeof(double));
-	}
-}
-
 static void init_mpi_cart_grid(){
 	NCOLS = 10;
 	NROWS = 10;
@@ -31,8 +9,8 @@ static void init_mpi_cart_grid(){
 	MPI_CART_DIMS[X_INDEX] = dims_tmp[X_INDEX];
 	MPI_CART_DIMS[Y_INDEX] = dims_tmp[Y_INDEX];
 	// TODO: ensure this works for different sizes
-	LOCAL_NROWS = NROWS / MPI_CART_DIMS[Y_INDEX];
-	LOCAL_NCOLS = NCOLS / MPI_CART_DIMS[X_INDEX];
+	LOCAL_NROWS = (NROWS / MPI_CART_DIMS[Y_INDEX]) + 2;
+	LOCAL_NCOLS = (NCOLS / MPI_CART_DIMS[X_INDEX]) + 2;
 	MPI_Cart_create(MPI_COMM_WORLD, 2, MPI_CART_DIMS, pbc, 0, &CART_COMM);
 	int coords[2];
 	MPI_Cart_coords(CART_COMM, RANK, 2, coords);
@@ -45,13 +23,12 @@ static void init(int argc, char *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &RANK);
 	MPI_Comm_size(MPI_COMM_WORLD, &NUM_NODES);
 	init_mpi_cart_grid();
-	init_recv_bufs();
 	init_grid();
 }
 
 // TODO: only create data type once at start
 // TODO: more send/recv pairs
-// TODO: vector recv is currently overwriting memory elsewhere
+// TODO: break this up into smaller functions
 static void exchange(){
 	MPI_Datatype col_vec;
 	MPI_Type_vector(LOCAL_NROWS, 1, LOCAL_NCOLS, MPI_DOUBLE, &col_vec);
@@ -63,33 +40,41 @@ static void exchange(){
 		printf("hey2\n");
 	}
 	if(LOCAL_X_COORD != 0){
-		//printf("Rank %d: want to send to left and receive from left\n", RANK);
+		// Send leftmost column to the node to our left in the topology
 		MPI_Request req;
 		int dest_rank;
 		int dest_coords[2] = { LOCAL_X_COORD - 1, LOCAL_Y_COORD };
 		MPI_Cart_rank(CART_COMM, dest_coords, &dest_rank);
-		MPI_Isend(&grid[0][0], 1, col_vec, dest_rank, 0, CART_COMM, &req);
+		MPI_Isend(&grid[1][1], 1, col_vec, dest_rank, 0, CART_COMM, &req);
+		// Receive left temp column from the node to our left in the topology
+		MPI_Status status;
+		int source_rank;
+		int source_coords[2] = { LOCAL_X_COORD - 1, LOCAL_Y_COORD };
+		MPI_Cart_rank(CART_COMM, source_coords, &source_rank);
+		MPI_Recv(&grid[1][0], 1, col_vec, source_rank, MPI_ANY_TAG, CART_COMM, &status);
 	}
 	if(LOCAL_X_COORD != MPI_CART_DIMS[X_INDEX] - 1){
+		// Send rightmost column to the node to our right in the topology
+		MPI_Request req;
+		int dest_rank;
+		int dest_coords[2] = { LOCAL_X_COORD + 1, LOCAL_Y_COORD };
+		MPI_Cart_rank(CART_COMM, dest_coords, &dest_rank);
+		MPI_Isend(&grid[1][LOCAL_NCOLS - 2], 1, col_vec, dest_rank, 0, CART_COMM, &req);
+		// Receive right temp column from the node to our right in the topology
 		MPI_Status status;
 		int source_rank;
 		int source_coords[2] = { LOCAL_X_COORD + 1, LOCAL_Y_COORD };
 		MPI_Cart_rank(CART_COMM, source_coords, &source_rank);
-		MPI_Recv(RIGHT_RECV_BUF, 1, col_vec, source_rank, MPI_ANY_TAG, CART_COMM, &status);
-		printf("Right recv buf: \n");
-		int i;
-		for(i = 0; i < LOCAL_NROWS * 10; i++){
-			printf("%f, ", RIGHT_RECV_BUF[i]);
-		}
-		printf("\n");
+		MPI_Recv(&grid[1][LOCAL_NCOLS-1], 1, col_vec, source_rank, MPI_ANY_TAG, CART_COMM, &status);
 	}
 	MPI_Type_free(&col_vec);
 }
 
 int main(int argc, char *argv[]){
 	init(argc, argv);
+	print_all_grids();
 	exchange();	
-	//print_all_grids();
+	print_all_grids();
 	MPI_Finalize();
 	return 0;
 }
